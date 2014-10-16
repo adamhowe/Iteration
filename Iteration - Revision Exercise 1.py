ae6edafd17d6c0369475b39d3b077d364b9cb4d1
Adam-Howe <40769@longroad.ac.uk>
2014-10-16 11:55:46 +0100

    Revision exercise

---
+++ Iteration - Revision Exercise 1.py
@@ -0,0 +1,8 @@
+#Adam Howe
+#Revision exercise 1
+#16/10/2014
+
+amount = int(input("Enter how many times you want to print 'Hello World'"))
+
+for counter in range(amount):
+    print("Hello World")
