6d24e960a62580131750f2c91dcac404c59ab6e8
Adam-Howe <40769@longroad.ac.uk>
2014-10-16 12:00:15 +0100

    Revision exercise

---
+++ Iteration - revision exercise 2.py
@@ -0,0 +1,10 @@
+#Adam Howe
+#Revision exercise 2
+#16/10/2014
+
+message = input("Enter the message you would like repeated: ")
+
+amount = int(input("Enter the amount of times you would like it repeated: "))
+
+for counter in range(amount):
+    print(message)
