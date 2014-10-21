15fa5a5e0dab7936ade19b01e6d3249b13eb03b6
Adam-Howe <40769@longroad.ac.uk>
2014-10-21 10:38:54 +0100

    Revision exercises

--- Iteration - Revision exercise 4.py
+++ Iteration - Revision exercise 4.py
@@ -7,7 +7,7 @@ number = int(input("Enter a number between 10 and 20: "))
 
 while not 10 <= number <= 20:  
     number = int(input("The number is outside the range, enter another one: "))
-    if 10<= number <= 20:
-        print("The number is within range")
+if 10<= number <= 20:
+    print("The number is within range")
 
     
