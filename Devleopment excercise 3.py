28f27ab1c81a064c33f25574976859d207a28ce5
Adam <Adam@AdamComputer>
2014-11-03 18:32:37 +0000

    Devlopment Exercise

---
+++ Devleopment excercise 3.py
@@ -0,0 +1,8 @@
+##for counter in range(1):
+##    string = int(input("Enter the message you would like printed backwards: "))
+##    print(string[::-1])
+
+number = 1
+while number != 0:
+    string = input("Enter the message you would like to be printed backwards: ")
+    print (string[::-1])
